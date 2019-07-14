3a976e3d4a3cd64a768ab83c412357c068456d72
risenforces <i.am@risen.dev>
2019-07-15 03:27:16 +0500

    Updates

---
+++ precache-manifest.a9a32a589ae1b4c2e07dec5c6b5a8a3d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96a00b6e30c5f8531e74ace4be887a3",
+    "url": "/cashkeeper/index.html"
+  },
+  {
+    "revision": "302b15483f9c2db3c1ac",
+    "url": "/cashkeeper/static/js/2.3ad8b3fd.chunk.js"
+  },
+  {
+    "revision": "fd5f8fdcd391d8440a3d",
+    "url": "/cashkeeper/static/js/main.fa2b3b81.chunk.js"
+  },
+  {
+    "revision": "96ddee0e5a70ce182987",
+    "url": "/cashkeeper/static/js/runtime~main.05ee521b.js"
+  }
+]);
